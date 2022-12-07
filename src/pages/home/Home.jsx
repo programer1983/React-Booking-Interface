abeb2c2834cfb05c037763ed5450129f970f7ca6
programer1983 <programer972@gmail.com>
2022-12-07 11:49:47 +0200

    Featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import "./home.css"
@@ -8,8 +9,12 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className='homeContainer'>
+        <Featured />
+        <h1 className='homeTitle'>Browse by property type</h1>
+      </div>
     </div>
   )
 }
 
-export default Home 
+export default Home    
